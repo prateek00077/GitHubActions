c4dc71bfd66a66d5a9f01052233f87f2c6ab871f
prateek00077 <pandeyprateeek560@gmail.com>
2026-03-19 16:50:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import e from "express";
+
+const app = e();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello I am Preateek's Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
